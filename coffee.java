cc8be3d54e3a30edbcd5ec85357e9c3284408b02
Nikhil <anvslhrnikhil@gmail.com>
2019-08-10 14:44:27 +0530

    Update coffee.java

--- coffee.java
+++ coffee.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+class Coffee
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int p = sc.nextInt();
+    int price = p;
+    int disc = sc.nextInt();
+    int result = price;
+    while(price != 0)
+    {
+      int  temp = price - (int)(disc/100)*price ; 
+      result += temp;
+      price = temp;
+    }
+    System.out.println(result);
+  }
+}
